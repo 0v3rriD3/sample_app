21017fc1cb55f1f2553bb5beda54e10c50737ae7
nullBr41n3r <khalebpr@gmail.com>
2012-02-16 21:25:56 -0500

    Finished static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ SampleApp::Application.routes.draw do
 
   get "static_pages/help"
 
+  get "static_pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -60,3 +62,4 @@ SampleApp::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 end
+
